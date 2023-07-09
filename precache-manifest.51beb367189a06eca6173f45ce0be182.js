ea736d3a9a9ff839d5eb3b19fc6388cacabe1fbb
desainimit <210133042048@silveroakuni.ac.in>
2023-07-10 00:49:09 +0530

    Updates

---
+++ precache-manifest.51beb367189a06eca6173f45ce0be182.js
@@ -0,0 +1,254 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f108e968b4d9fc91efefa950b6f445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31167f2fb1ca581e6d9b",
+    "url": "/static/css/2.f0c7496f.chunk.css"
+  },
+  {
+    "revision": "c78b272fc0a5e0c4b5c2",
+    "url": "/static/css/main.4b255023.chunk.css"
+  },
+  {
+    "revision": "31167f2fb1ca581e6d9b",
+    "url": "/static/js/2.a6babdc8.chunk.js"
+  },
+  {
+    "revision": "fe438f42bfcd7b3c4088f83b074b4b11",
+    "url": "/static/js/2.a6babdc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c78b272fc0a5e0c4b5c2",
+    "url": "/static/js/main.c4e8e66b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "131bc5f4b19843fb8394f6d56bb83a81",
+    "url": "/static/media/SPS_logo.131bc5f4.png"
+  },
+  {
+    "revision": "d21f485d843f7c4550e2a27106cda5ee",
+    "url": "/static/media/about1.d21f485d.jpg"
+  },
+  {
+    "revision": "01166b2edcfc1430dfcad3ec9322f240",
+    "url": "/static/media/about2.01166b2e.jpg"
+  },
+  {
+    "revision": "51d907fdd86d7c5ffa6fb890d949bc93",
+    "url": "/static/media/about3.51d907fd.jpg"
+  },
+  {
+    "revision": "c0cae4c54ba4614b2c251b1f0665ec26",
+    "url": "/static/media/about4.c0cae4c5.jpg"
+  },
+  {
+    "revision": "30bf80c872a4006f15b4e015881b9fb5",
+    "url": "/static/media/author3.30bf80c8.jpg"
+  },
+  {
+    "revision": "7fc72d5fbc682ddb3baf8c922de7529c",
+    "url": "/static/media/author4.7fc72d5f.jpg"
+  },
+  {
+    "revision": "c37d1aa39cccb5026c7876de1f8450ed",
+    "url": "/static/media/author5.c37d1aa3.jpg"
+  },
+  {
+    "revision": "a3641071fc38019d5720218550fe3f8b",
+    "url": "/static/media/author6.a3641071.jpg"
+  },
+  {
+    "revision": "5a036f954eda80c921c587e992f2ef78",
+    "url": "/static/media/author7.5a036f95.jpg"
+  },
+  {
+    "revision": "35f49d935427a72f2ef29b905057ca82",
+    "url": "/static/media/author8.35f49d93.jpg"
+  },
+  {
+    "revision": "ccd1d5b6e53562b92075d2d23648512f",
+    "url": "/static/media/author9.ccd1d5b6.jpg"
+  },
+  {
+    "revision": "936f5cceb30a4a7873ffa4bc5a8042f3",
+    "url": "/static/media/bg.936f5cce.jpg"
+  },
+  {
+    "revision": "35a8efa71eb1d85e6160a84524bf9807",
+    "url": "/static/media/blog1.35a8efa7.jpg"
+  },
+  {
+    "revision": "c1b744d3a0158ebb4b5dbac8edfd21ba",
+    "url": "/static/media/blog2.c1b744d3.jpg"
+  },
+  {
+    "revision": "bf68c01d6c8451f523436cd8d32f64aa",
+    "url": "/static/media/blog3.bf68c01d.jpg"
+  },
+  {
+    "revision": "a2a584b2518fe3585f25b8fbee0ff460",
+    "url": "/static/media/blog4.a2a584b2.jpg"
+  },
+  {
+    "revision": "5719df0e9a3ad251a0707d03a1fd1403",
+    "url": "/static/media/blog5.5719df0e.jpg"
+  },
+  {
+    "revision": "6a2ab56aa501070c3c546b0103e58c5b",
+    "url": "/static/media/blog6.6a2ab56a.jpg"
+  },
+  {
+    "revision": "c8e23bcd8f97ccabb8b584432e788459",
+    "url": "/static/media/buy-tickets-bg.c8e23bcd.jpg"
+  },
+  {
+    "revision": "b1c923df0d992d5cae749bc3017733fc",
+    "url": "/static/media/color-bg.b1c923df.jpg"
+  },
+  {
+    "revision": "6f25e7bb61556eb47e41ac006b21db1d",
+    "url": "/static/media/funfacts-bg.6f25e7bb.jpg"
+  },
+  {
+    "revision": "324fd56cede0b34bc875232dc0d287de",
+    "url": "/static/media/gold-partner2.324fd56c.png"
+  },
+  {
+    "revision": "d97e8f8277d1561e707f3659b6971606",
+    "url": "/static/media/gold-partner5.d97e8f82.png"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "48c9a92c77244b4522250e00990a98b8",
+    "url": "/static/media/main-bg1.48c9a92c.jpg"
+  },
+  {
+    "revision": "fefe3c450690811a2715eae6859247e1",
+    "url": "/static/media/main-bg2.fefe3c45.jpg"
+  },
+  {
+    "revision": "d383d7efd560277e148645a5c4c51723",
+    "url": "/static/media/main-bg3.d383d7ef.jpg"
+  },
+  {
+    "revision": "1e2e3fabec1f0393066364cdfe00496c",
+    "url": "/static/media/platinum-partner1.1e2e3fab.png"
+  },
+  {
+    "revision": "b38b5dfe7073502abacd4ee045abf604",
+    "url": "/static/media/platinum-partner2.b38b5dfe.png"
+  },
+  {
+    "revision": "efc06ddae92968a58676647b3d65850c",
+    "url": "/static/media/platinum-partner3.efc06dda.png"
+  },
+  {
+    "revision": "31fbd2d758717a6737121c09a0d9a4ff",
+    "url": "/static/media/platinum-partner4.31fbd2d7.png"
+  },
+  {
+    "revision": "b6cd9577fde830a00fc6c5606d9ff9f2",
+    "url": "/static/media/platinum-partner5.b6cd9577.png"
+  },
+  {
+    "revision": "473a71abc0251adc04cd352252357e52",
+    "url": "/static/media/promo-video.473a71ab.mp4"
+  },
+  {
+    "revision": "76732e633ea7984bac8742675220e989",
+    "url": "/static/media/slider-bg1.76732e63.jpg"
+  },
+  {
+    "revision": "e5f6a7251cecff0a58fcb710ce953eb1",
+    "url": "/static/media/slider-bg2.e5f6a725.jpg"
+  },
+  {
+    "revision": "c482f926d2a7a2fb16d748963f8a4eab",
+    "url": "/static/media/slider-bg3.c482f926.jpg"
+  },
+  {
+    "revision": "d383d7efd560277e148645a5c4c51723",
+    "url": "/static/media/slideshow-bg1.d383d7ef.jpg"
+  },
+  {
+    "revision": "e7dcb1e675a550d994f7c386aebb6777",
+    "url": "/static/media/slideshow-bg2.e7dcb1e6.jpg"
+  },
+  {
+    "revision": "48c9a92c77244b4522250e00990a98b8",
+    "url": "/static/media/slideshow-bg3.48c9a92c.jpg"
+  },
+  {
+    "revision": "2f621370906f2d9fccd9a0e8c9a5d35b",
+    "url": "/static/media/slideshow-bg4.2f621370.jpg"
+  },
+  {
+    "revision": "56808d76cc4af6dad87f0f8bf584c035",
+    "url": "/static/media/sou-logo.56808d76.png"
+  },
+  {
+    "revision": "d0c7bf432fc53e6a74953b98f32ff930",
+    "url": "/static/media/speakers1.d0c7bf43.jpg"
+  },
+  {
+    "revision": "f6ff943a7feaf2821c5798ce14da74e5",
+    "url": "/static/media/speakers2.f6ff943a.jpg"
+  },
+  {
+    "revision": "bd26e8f11dc94b2d9740b56f9752b00a",
+    "url": "/static/media/speakers3.bd26e8f1.jpg"
+  },
+  {
+    "revision": "b5685b5473ee23a73037c71d8a5c1e65",
+    "url": "/static/media/speakers4.b5685b54.jpg"
+  },
+  {
+    "revision": "b2a63333aa4456248e1d01d2bbb07e8a",
+    "url": "/static/media/speakers5.b2a63333.jpg"
+  },
+  {
+    "revision": "4d8a75df9390d8437ff9dec725a8d52e",
+    "url": "/static/media/speakers6.4d8a75df.jpg"
+  },
+  {
+    "revision": "a86383ebc9e397a406d1bd5704b9f024",
+    "url": "/static/media/speakers7.a86383eb.jpg"
+  },
+  {
+    "revision": "7605392d6cab0d5c0deb493bfe85dd10",
+    "url": "/static/media/speakers8.7605392d.jpg"
+  },
+  {
+    "revision": "6c7bf87bfeb9ec8d84a8bf63a9eb7eba",
+    "url": "/static/media/sps-gujarat-section.6c7bf87b.png"
+  },
+  {
+    "revision": "2e5f1151f6e8a6d54c7c4d9ce1e2e337",
+    "url": "/static/media/sps_sou.2e5f1151.png"
+  },
+  {
+    "revision": "5ecc8b90765922401859c6733f0a101d",
+    "url": "/static/media/why-choose-img1.5ecc8b90.jpg"
+  },
+  {
+    "revision": "95b514f48b916148e1354bed1d3e66bd",
+    "url": "/static/media/why-choose-img2.95b514f4.jpg"
+  },
+  {
+    "revision": "c001363acce714b03ce80441adebf76b",
+    "url": "/static/media/why-choose-img3.c001363a.jpg"
+  }
+]);
