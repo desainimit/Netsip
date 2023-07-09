812a312ab0725277756ddce493119e006513e261
desainimit <210133042048@silveroakuni.ac.in>
2023-07-10 01:02:01 +0530

    github pages

--- evnia/client/src/Routes.jsx
+++ evnia/client/src/Routes.jsx
@@ -44,20 +44,10 @@ const AppRouter = () => {
             {renderNavigation()}
             <Switch>
                 <Route path="/" exact component={HomeDefault} />
-                <Route path="/home-two" exact component={HomeTwo} />
-                <Route path="/home-three" exact component={HomeThree} />
-                <Route path="/home-four" exact component={HomeFour} />
-                <Route path="/home-five" exact component={HomeFive} />
                 <Route path="/about-1" exact component={AboutOne} />
-                <Route path="/about-2" exact component={AboutTwo} />
-                <Route path="/about-3" exact component={AboutThree} />
                 <Route path="/pricing-1" exact component={PricingOne} />
                 <Route path="/pricing-2" exact component={PricingTwo} />
                 <Route path="/speakers-1" exact component={SpeakersOne} />
-                <Route path="/speakers-2" exact component={SpeakersTwo} />
-                <Route path="/speakers-3" exact component={SpeakersThree} />
-                <Route path="/schedule-1" exact component={ScheduleOne} />
-                <Route path="/schedule-2" exact component={ScheduleTwo} />
                 <Route path="/schedule-3" exact component={ScheduleThree} />
                 <Route path="/schedule-4" exact component={ScheduleFour} />
                 <Route path="/login" exact component={Login} />
@@ -65,11 +55,6 @@ const AppRouter = () => {
                 <Route path="/coming-soon" exact component={ComingSoon} />
                 <Route path="/faq" exact component={Faq} />
                 <Route path="/contact" exact component={Contact} />
-                <Route path="/blog-1" exact component={BlogOne} />
-                <Route path="/blog-2" exact component={BlogTwo} />
-                <Route path="/blog-3" exact component={BlogThree} />
-                <Route path="/blog-4" exact component={BlogFour} />
-                <Route path="/single-blog" exact component={BlogDetails} />
                 <Route path="/sponsors" exact component={Sponsor} />
                 <Route path="/error-404" exact component={NotFound} />
                 <Route component={NotFound} />
